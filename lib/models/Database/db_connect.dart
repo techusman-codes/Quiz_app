5356bf1bbe7274b284aaf6a949b563c404a2103b
Usman Umar Garba <ugarba202@gmail.com>
2025-06-02 23:52:41 +0100

    adding a database

---
+++ lib/models/Database/db_connect.dart
@@ -0,0 +1,8 @@
+import 'package:demo_app/models/Questions/qustions_models.dart';
+import 'package:http/http.dart' as http; // the http package
+
+class DbConnect {
+  // lest first create a function to add a Question to our database
+
+  Future<void> addQuestion(Question question) async {}
+}
